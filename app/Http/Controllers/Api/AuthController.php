0bfd7c580da877be578761a3ebb23fb824b72f84
Eugene <medov-foto@mail.ru>
2026-03-21 20:30:47 +0300

    реализовал login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -59,4 +59,18 @@ class AuthController extends Controller
 
         return response()->noContent();
     }
+
+    /**
+     * Handle a logout request.
+     */
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
 }
